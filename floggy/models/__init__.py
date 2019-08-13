e050d86bb2d3b1c06208ad28722aef0c4eed4298
Ilya Chernyavsckiy <ilya.cher.3@gmail.com>
2019-08-13 23:42:26 +0300

    [master] Models core

--- floggy/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from floggy.models.User import User
-from floggy.models.Setting import Setting
-from floggy.models.ContentType import ContentType
-from floggy.models.Comment import Comment
-from floggy.models.Rubric import Rubric
